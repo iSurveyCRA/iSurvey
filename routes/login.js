b7339cf734ca12a9b38b27ad22920c3c807fb168
Min Young Lee <avadaisuki68@gmail.com>
2018-07-19 11:02:16 +0900

    Added logout route and checking session

--- routes/login.js
+++ routes/login.js
@@ -4,10 +4,16 @@ var router = express.Router();
 var login_controller = require('../controllers/loginController');
 
 // GET login page
+// 만일 session의 userId가 존재한다면 바로 홈페이지로 보낸다.
 router.get('/', function(req, res, next){
-	res.render('login');
+	if(!req.session.userId)
+		res.render('login');
+	else
+		res.redirect('/');
 });
 /* POST Login Function */
 router.post('/login', login_controller.login);
+
+router.get('/logout', login_controller.logout);
 
 module.exports = router;
