71d0cd414a0aba459d1ae20447c80af5d380a647
hyewon <db3p@naver.com>
2018-08-05 22:46:48 +0900

    save according to model schema

--- controllers/formController.js
+++ controllers/formController.js
@@ -1,32 +1,38 @@
+var Form = require('../models/forms');
+var Search = require('../models/search');
+var User = require('../models/user');
+
+var async = require('async');
+
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var url = 'mongodb://localhost:27017/iSurveyTest';
 
-var insertDocuments = function(db, query, callback){
-  var collection = db.collection('forms');
-
-  collection.insertOne(query, function(err,result){
-    assert.equal(err,null);
-    assert.equal(1, result.result.n);
-    assert.equal(1, result.ops.length);
-    console.log("Inserted one account to 'forms' collection");
-    callback(result);
-  });
-}
-
-//exports.saveForm = function(req, res, next){
-//	console.log(req.body.Json);
-//}
 
 //form 저장하기
-exports.saveTomongo = function(req, res, next) {
+exports.saveForm = function(req, res, next) {
 	//xhr.send에서 JSON.stringify로보내기 때문에 다시 parse해주어야 한다. 
 	var data = JSON.parse(req.body.Json);
-	MongoClient.connect(url, function(err, db){
-		assert.equal(err,null);
-		console.log("Successfully connected to DB");
-		insertDocuments(db, data ,function(){
-			 db.close();
-		});
+
+	 async.parallel({
+                user: function(callback){
+                        User.findOne({ '_id':req.session.userId}).exec(callback);
+                },
+        }, function(err, results){
+		if (err) { return next(err); }
+			var formData = result.user;	
+			var formInfo = new Form({
+                        	username: formData.username,
+                        	student_id: formData.student_id,
+                       		user_department: formData.user_department,
+                	//      date: formData.date,
+                        	data: data
+                	});
+        		formInfo.save(function(err) {
+                	if(err) { res.render('result', {result: 'Failed'}); }
+                	else { console.log("Inserted one account to 'forms' collection");}
+        		});
+
 	});
-}
+	
+};
