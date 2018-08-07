633bec1d1df0a4767bf680ddc852c5cc3a23eb95
MinYoung Lee <avadaisuki68@gmail.com>
2018-08-07 15:31:10 +0900

    Added Student Name and Student Id in Session

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -67,6 +67,8 @@ exports.login = function(req, res, next){
 					if (err) { res.render('result', {result:'Failed'}); }
 				});
 				req.session.userId = userInfo._id;
+				req.session.userName = userInfo.name;
+				req.session.userNumber = userInfo.student_id;
 				res.redirect('/');
 			});
 		} else {
@@ -74,6 +76,8 @@ exports.login = function(req, res, next){
 			if (ans==req.body.password){
 				// 사용자의 session의 userId라는 필드에 현재 로그인한 사용자의 ObjectId를 저장한다.
 				req.session.userId = results.user._id;
+				req.session.userName = results.user.name;
+				req.session.userNumber = results.user.student_id;
 	 			res.redirect('/'); 
 			} else { 
 				res.render('result', {result:'Wrong Password'}); 
