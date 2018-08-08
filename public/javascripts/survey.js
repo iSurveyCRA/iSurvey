de3626076534fee81d6f926115ab9e20e7871d22
hyewon <db3p@naver.com>
2018-08-08 17:32:48 +0900

    before json import

--- public/javascripts/survey.js
+++ public/javascripts/survey.js
@@ -2,11 +2,12 @@
 
 Survey.Survey.cssType = "winter";
 var surveyJSON = { questions: [ {type:"text", name: "question1" } ] };
-//var surveyJSON = forminfo.data; 여기에 data가져오기
+//var surveyJSON = forminfo.data;// 여기에 data가져오기
 
 //function sendDataToServer(survey){
 //      alert("The results are:" + JSON.stringify(survey.data));
 //}
+
 
 var survey = new Survey.Model(surveyJSON);
 $("#surveyContainer").Survey({
