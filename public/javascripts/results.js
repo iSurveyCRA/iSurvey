0b86f31914ce90b2c3da1cec334fc1a507e60dde
hyewon <db3p@naver.com>
2018-11-07 04:35:49 +0900

    설문에 응답한 user&학부 나타내기

--- public/javascripts/results.js
+++ public/javascripts/results.js
@@ -3,14 +3,14 @@ console.log(jsonResult.replace(/&quot;/g,"\""));
 
 var surveyJSONFromDB =jsonData.replace(/&quot;/g,"\"") ;//작성폼 json
 var surveyResultsDataFromDB =jsonResult.replace(/&quot;/g,"\"") ;//답변 json
+var obj = JSON.parse(surveyResultsDataFromDB);
 
-function surveyResultModel(id, student_id, department, results) {
+function surveyResultModel(id, student_id, user_department, results) {
   var self = this;
   self.id = id;
   self.student_id = student_id;
-  self.department = department;
-//  self.postedAt = postedAt;
-  self.results = results;
+  self.user_department = user_department;
+  self.data = results;
   self.jsonResultsValue = null;
   self.getJsonResults = function () {
     if (!self.jsonResultsValue && self.results) {
@@ -23,28 +23,29 @@ function surveyResultsModel(data) {
   var self = this;
   var items = [];
   if (data) {
-    for (var i = 0; i < data.length; i++) {
+    for (var i = 0; i < Object.keys(data).length; i++) {
       var item = data[i];
-      items.push(new surveyResultModel(i + 1, item.student_id, item.department, item.results));
-    }
+	console.log(item.data);
+      items.push(new surveyResultModel(i + 1, item.student_id, item.user_department, item.data));
   }
+}
   self.koItems = ko.observableArray(items);
   self.showSurveyResult = function (item) {
     survey.clear();
     survey.data = item.getJsonResults();
-    document.getElementById("surveyResultModalTitle").innerHTML = "Show result for: " + item.name;
+    document.getElementById("surveyResultModalTitle");
     $("#surveyResultModal").modal();
   }
 }
-ko.applyBindings(new surveyResultsModel(surveyResultsDataFromDB), document.getElementById("resultsTable"));
+ko.applyBindings(new surveyResultsModel(obj), document.getElementById("resultsTable"));
 
-//Survey.Survey.cssType = "bootstrap";
-Survey
-    .StylesManager
-    .applyTheme("winter");
+Survey.Survey.cssType = "bootstrap";
+//Survey
+//    .StylesManager
+//    .applyTheme("winter");
 
-var json = JSON.parse(surveyJSONFromDB);
+var json = surveyJSONFromDB;
+//var json = JSON.parse(surveyJSONFromDB);
 var survey = new Survey.Model(json);
 survey.mode = 'display';
 survey.render("surveyElement");
-
