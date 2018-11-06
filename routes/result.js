8770838b3e3fad1602ec1ae9f5141c4d2c63ad9b
hyewon <db3p@naver.com>
2018-11-07 00:45:51 +0900

    Try uploading result

--- routes/result.js
+++ routes/result.js
@@ -3,23 +3,34 @@ var router  = express.Router();
 var async = require('async');
 
 var Result = require('../models/results');
+var Form = require('../models/forms');
 
-//router.route('/:id')
-router.route('/mypage/result/:id')
+router.route('/:id')
 	.get(function(req, res, next){
-	async.parallel({
-		result: function(callback){
-			Result.findById(req.params.id).exec(callback);
+//	console.log(req.params.id);
+	 async.parallel({
+                result: function(callback){
+               //        Result.findById(req.params.id).exec(callback);
+			Result.find({ '_formid':req.params.id}).exec(callback);
 		},
-	}, function(err, results){
-		if(err) {return next(err);}
-		if(results.result == null){
-			var err = new Error('Result not found');
-			err.status = 404;
-			return next(err);
-		}
+		survey: function(callback){
+			Form.findById(req.params.id).exec(callback);
+                },
+        }, function(err, results){
+                if(err) { return next(err); }
+                if(results.result == null){
+                        var err = new Error('Survey not found');
+                        err.status = 404;
+                        return next(err);
+                }
 		console.log("결과: ");
-		console.log(results.result.data);
-		res.render('result', {resultinfo: results.result});
-	});
+		console.log(results.result);
+		res.render('result', {resultinfo: results.result, forminfo:results.survey});
+        });
+
+ 
 });
+
+
+module.exports = router;
+
