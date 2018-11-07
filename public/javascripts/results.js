a11fe0da66445425fca7be133e46e49e28a4dec5
hyewon <db3p@naver.com>
2018-11-07 22:53:51 +0900

    Loading Result

--- public/javascripts/results.js
+++ public/javascripts/results.js
@@ -1,22 +1,18 @@
-console.log(jsonData.replace(/&quot;/g,"\""));
-console.log(jsonResult.replace(/&quot;/g,"\""));
-
-var surveyJSONFromDB =jsonData.replace(/&quot;/g,"\"") ;//작성폼 json
-var surveyResultsDataFromDB =jsonResult.replace(/&quot;/g,"\"") ;//답변 json
+//응답자 별 결과보기
+var surveyJSONFromDB =jsonData.replace(/&quot;/g,"\"") ;//작성폼 json(object)
+var surveyResultsDataFromDB =jsonResult.replace(/&quot;/g,"\"") ;//답변 json(array)
 var obj = JSON.parse(surveyResultsDataFromDB);
 
-function surveyResultModel(id, student_id, user_department, results) {
+var survey = new Survey.Model(surveyJSONFromDB);
+
+function surveyResultModel(id, student_id, user_department, data) {
   var self = this;
   self.id = id;
   self.student_id = student_id;
   self.user_department = user_department;
-  self.data = results;
-  self.jsonResultsValue = null;
+  self.data = data;
   self.getJsonResults = function () {
-    if (!self.jsonResultsValue && self.results) {
-      self.jsonResultsValue = JSON.parse(self.results);
-    }
-    return self.jsonResultsValue;
+	return self.data;
   }
 }
 function surveyResultsModel(data) {
@@ -31,21 +27,17 @@ function surveyResultsModel(data) {
 }
   self.koItems = ko.observableArray(items);
   self.showSurveyResult = function (item) {
-    survey.clear();
+//    survey.clear();
     survey.data = item.getJsonResults();
     document.getElementById("surveyResultModalTitle");
     $("#surveyResultModal").modal();
+survey.mode = 'display';
+survey.render("surveyElement");
   }
 }
 ko.applyBindings(new surveyResultsModel(obj), document.getElementById("resultsTable"));
 
-Survey.Survey.cssType = "bootstrap";
-//Survey
-//    .StylesManager
-//    .applyTheme("winter");
-
-var json = surveyJSONFromDB;
-//var json = JSON.parse(surveyJSONFromDB);
-var survey = new Survey.Model(json);
-survey.mode = 'display';
-survey.render("surveyElement");
+//Survey.Survey.cssType = "bootstrap";
+Survey
+	.StylesManager
+	.applyTheme("winter");
